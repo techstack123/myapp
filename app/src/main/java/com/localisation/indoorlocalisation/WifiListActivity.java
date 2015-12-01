cc5436a15e6897763b08bcc2fe1c441337692da8
praveen kumar <praveenkmr.t@gmail.com>
2015-12-01 11:51:42 +0530

    Compute function added

--- app/src/main/java/com/localisation/indoorlocalisation/WifiListActivity.java
+++ app/src/main/java/com/localisation/indoorlocalisation/WifiListActivity.java
@@ -15,7 +15,14 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.TreeSet;
 
 /**
  * Created by Praveen on 11/25/2015.
@@ -26,35 +33,83 @@ public class WifiListActivity extends AppCompatActivity {
     WifiManager mWifi;
     ListView wifiListView;
     WifiListAdapter wifiListAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wifi_list);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#E43F3F")));
         getSupportActionBar().setTitle("WIFI LIST");
-        wifiListView=(ListView)findViewById(R.id.wifiList);
-        mWifi= (WifiManager)getSystemService(Context.WIFI_SERVICE);
+        wifiListView = (ListView) findViewById(R.id.wifiList);
+        mWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         registerReceiver(w, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         addItemsToList();
-        if(!mWifi.isWifiEnabled()) {
+        if (!mWifi.isWifiEnabled()) {
             removeItemsfromList();
             Toast.makeText(getApplicationContext(), "Please enable WIFI", Toast.LENGTH_LONG).show();
         }
-            mWifi.startScan();
-            addItemsToList();
-        }
-
-
-
-    public void startScan(View v){
-        if(!mWifi.isWifiEnabled()) {
-            removeItemsfromList();
-            Toast.makeText(getApplicationContext(), "Please enable WIFI", Toast.LENGTH_LONG).show();
-        }
-            mWifi.startScan();
-            addItemsToList();
+        mWifi.startScan();
+        addItemsToList();
     }
-    public void removeItemsfromList(){
+
+
+    public void startScan(View v) {
+        if (!mWifi.isWifiEnabled()) {
+            removeItemsfromList();
+            Toast.makeText(getApplicationContext(), "Please enable WIFI", Toast.LENGTH_LONG).show();
+        }
+        mWifi.startScan();
+        addItemsToList();
+    }
+    public String loadJSONFromAsset(String fileName) {
+        String json = null;
+        try {
+            InputStream is = getAssets().open(fileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+    public  void computeFingerPrintDistance(HashMap<String,Integer> referenceData,HashMap<String,Integer> measuredData)
+    {
+        int total=0;
+        for (String keyName:referenceData.keySet()){
+            if(measuredData.get(keyName)!=null){
+                Log.d("BSSID",referenceData.get(keyName)+","+measuredData.get(keyName));
+                int diff=(referenceData.get(keyName)-measuredData.get(keyName));
+                diff=diff*diff;
+                total=total+diff;
+            }
+        }
+        Log.d("bssid",""+total);
+    }
+    public HashMap<String,Integer> getFingerPrintDataFromJson(String fileName){
+        HashMap<String,Integer> fingerPrintData=new HashMap<String, Integer>();
+        String JsonValue = loadJSONFromAsset(fileName);
+//        Log.d("Json test",JsonValue)
+        try {
+            JSONObject JO = new JSONObject(JsonValue);
+            JSONObject ap = JO.getJSONObject("measurement 4");
+            for (int j=1;j< ap.length();j++) {
+                String apObject="ap"+j;
+                JSONObject aps = ap.getJSONObject(apObject);
+                fingerPrintData.put(aps.getString("BSSID"),aps.getInt("level"));
+            }
+        } catch (JSONException e) {
+
+        }
+        return fingerPrintData;
+    }
+    public void removeItemsfromList() {
+
+        computeFingerPrintDistance(getFingerPrintDataFromJson("measureData1.json"),getFingerPrintDataFromJson("measureData2.json"));
         for (int i=0;i<wifiList.size();i++)
         {
             wifiList.remove(i);
@@ -62,9 +117,10 @@ public class WifiListActivity extends AppCompatActivity {
         wifiListAdapter = new WifiListAdapter(this, wifiList);
         wifiListView.setAdapter(wifiListAdapter);
     }
-    public void addItemsToList(){
 
-        wifiList=mWifi.getScanResults();
+    public void addItemsToList() {
+
+        wifiList = mWifi.getScanResults();
         wifiListAdapter = new WifiListAdapter(this, wifiList);
         wifiListView.setAdapter(wifiListAdapter);
     }
@@ -72,7 +128,7 @@ public class WifiListActivity extends AppCompatActivity {
     class WifiReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            wifiList=mWifi.getScanResults();
+            wifiList = mWifi.getScanResults();
             for (int i = 0; i < wifiList.size(); i++) {
 //                sensorList.add(""+wifiList.get(i).toString());
                 Log.d("Wifi", wifiList.get(i).toString());
